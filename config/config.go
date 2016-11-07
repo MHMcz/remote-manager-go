99986612880c6e02ac78da1f781dbbd1b704a75a
Jan Markup <mhmcze@gmail.com>
2016-11-07 22:34:40 +0100

    add license

--- config/config.go
+++ config/config.go
@@ -1,3 +1,10 @@
+/*
+	Copyright © 2016 Jan Markup <mhmcze@gmail.com>
+	This work is free. You can redistribute it and/or modify it under the
+	terms of the Do What The Fuck You Want To Public License, Version 2,
+	as published by Sam Hocevar. See the COPYING file for more details.
+*/
+
 package config
 
 import (
